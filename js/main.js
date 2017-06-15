3e00d9a002273c9160e31aa4ae8df75774f0c621
Ash Duckett <ash.duckett@outlook.com>
2017-06-15 02:04:29 +0100

    Updated portfolio

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
-    console.log('We are off!');
 
     $(window).scroll(function() {
-        console.log('scrolling!');
         var scrollTop = $(document).scrollTop();
-        console.log('scrollTop is ' + scrollTop);
 
         if(scrollTop >= 300) {
-            console.log('hitting');
             $('.s').addClass('shrunk-nav');
             $('.brand h2').addClass('shrunk-brand');
         } else {
             $('.s').removeClass('shrunk-nav');
             $('.brand h2').removeClass('shrunk-brand');
         }
+    });
+
+    $('.portfolio-item').mouseover(function() {
+        $(this).children('.portfolio-item-overlay').css('opacity', '0.7');
+        $(this).children('.portfolio-image').addClass('portfolio-item-zoomed');
+    });
+
+    $('.portfolio-item').mouseleave(function() {
+        $(this).children('.portfolio-item-overlay').css('opacity', '0');
+        $(this).children('.portfolio-image').removeClass('portfolio-item-zoomed');
+
     });
 });
