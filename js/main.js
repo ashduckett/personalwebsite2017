15a9e45e7c59d4152c80a314ed6fcda085a4e2b6
Ash Duckett <ash.duckett@outlook.com>
2017-07-13 04:37:07 +0100

    Swift projects added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,124 @@
 $(document).ready(function() {
+    // This could probably just be stored in json. Maybe even a database.
+    let PortfolioItem = function(title, screenshotUrl, techUsed, linkLive, linkGithub, desc) {
+        this.title = title
+        this.screenshotUrl = screenshotUrl
+        this.techUsed = techUsed
+        this.linkLive = linkLive
+        this.linkGithub = linkGithub
+        this.desc = desc
+    };
+
+    let frogger = new PortfolioItem('Frogger', 'img/frogger.png', 'OOP JavaScript/Canvas', 'http://www.ashduckett.com/frogger', 'https://github.com/ashduckett/FEND-Frogger', 'Description');
+    let krays = new PortfolioItem('Kray Twins Atlas', 'img/krays.png', 'OOP JavaScript/Canvas', 'http://www.ashduckett.com/kraymap', 'https://github.com/ashduckett/Neighbourhood-Map', 'Description');
+    let shout = new PortfolioItem('Shout', 'img/shout.png', 'PHP/JS/jQuery', '#', 'https://github.com/ashduckett/shout', 'Description');
+    let ashios = new PortfolioItem('Ashios', 'img/ashios.png', 'Docs/jQuery', '#', 'https://github.com/ashduckett/ashios', 'Description');
+    let myreads = new PortfolioItem('My Reads', 'img/myreads.png', 'React/ECMAScript6', 'https://fast-tundra-20236.herokuapp.com/', 'https://github.com/ashduckett/MyReads', 'Description');
+
+    let PortfolioSet = function(title, items) {
+        this.title = title
+        this.items = items
+    };
+
+
+    PortfolioSet.prototype.createLink = function(href, iconName) {
+        let portfolioUrl = $(document.createElement('div'));
+        portfolioUrl.addClass('portfolio-url');
+
+        let span = $(document.createElement('span'));
+        let anchor = $(document.createElement('a'));
+
+        anchor.prop('href', href)
+        anchor.prop('target', '_blank');
+
+        let iThing = $(document.createElement('i'));
+        iThing.addClass('fa');
+        iThing.addClass(iconName);
+        iThing.prop('aria-hidden', 'true');
+
+        anchor.append(iThing);
+
+        portfolioUrl.append(span);
+        span.append(anchor);
+        return portfolioUrl;
+
+    }
+
+    PortfolioSet.prototype.draw = function(element) {
+        // Create the main header
+        element.append('<h2>' + this.title + '</h2>');
+
+        let portfolioRow = $(document.createElement('div'));
+        portfolioRow.addClass('portfolio-row');
+
+        for (item of this.items) {
+
+            // To the portfolio row, we add a div called portfolio item
+            let portfolioItem = $(document.createElement('div'));
+            portfolioItem.addClass('portfolio-item');
+            portfolioRow.append(portfolioItem);
+
+            // Create an image
+            let portfolioImage = $(document.createElement('img'));
+            portfolioImage.addClass('portfolio-image');
+            portfolioImage.prop('src', item.screenshotUrl);
+
+            let portfolioOverlay = $(document.createElement('div'));
+            portfolioOverlay.addClass('portfolio-item-overlay');
+
+            let links = $(document.createElement('div'));
+
+            let portfolioOverlayDiv = $(document.createElement('div'));
+            portfolioOverlayDiv.addClass('links');
+
+            portfolioOverlayDiv.append('<h3>' + item.title + '</h3>');
+            portfolioOverlayDiv.append('<h4>' + item.techUsed + '</h4>');
+
+
+            let liveLink 
+            
+            if(item.linkLive !== null) {
+                liveLink = this.createLink(item.linkLive, 'fa-play');
+                portfolioOverlayDiv.append(liveLink);
+            }
+
+            
+            let githubLink = this.createLink(item.linkGithub, 'fa-github');
+            let descLink = this.createLink(item.linkGithub, 'fa-info-circle');
+
+            
+            portfolioOverlayDiv.append(githubLink);
+            portfolioOverlayDiv.append(descLink);
+            portfolioOverlay.append(portfolioOverlayDiv);
+
+            portfolioItem.append(portfolioImage);
+            portfolioItem.append(portfolioOverlay);
+
+            portfolioRow.append(portfolioItem);
+        }
+
+        element.append(portfolioRow);
+    };
+
+    let webPortfolioSet = new PortfolioSet('Web', [frogger, krays, shout, ashios, myreads]);
+    webPortfolioSet.draw($('section#portfolio .container'));
+
+
+    let onTheMap = new PortfolioItem('On the Map', 'img/onTheMap.png', 'Swift/iOS/MapKit', null, 'https://github.com/ashduckett/FEND-Frogger', 'Description');
+    let virtualTourist = new PortfolioItem('Virtual Tourist', 'img/virtualTouristOnPhone.png', 'Swift/iOS/MapKit', null, 'https://github.com/ashduckett/FEND-Frogger', 'Description');
+    let pitchPerfect = new PortfolioItem('Pitch Perfect', 'img/pitchPerfectOnPhone.png', 'Autolayout/Swift/iOS', null, 'https://github.com/ashduckett/FEND-Frogger', 'Description');
+    let memeMe = new PortfolioItem('Meme Me', 'img/memeMeOnPhone.png', 'Autolayout/Swift/iOS', null, 'https://github.com/ashduckett/FEND-Frogger', 'Description');
+
+    let iosPortfolioSet = new PortfolioSet('iOS', [onTheMap, virtualTourist, pitchPerfect, memeMe]);
+    iosPortfolioSet.draw($('section#portfolio .container'));
+    
     // Disable send button initially
     $('.submit-contact').prop('disabled', true);
-    
+
     function isEmail(email) {
         var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         return regex.test(email);
     }
-
 
     $(window).scroll(function() {
         var scrollTop = $(document).scrollTop();
@@ -91,7 +203,6 @@ $(document).ready(function() {
         validateFormInput();
     });
 
-
     $('#portfolio-link').click(function() {
         $('html, body').animate({
             scrollTop: $('section#portfolio').offset().top - 80
@@ -109,10 +220,4 @@ $(document).ready(function() {
             scrollTop: $('section#contact').offset().top - 80
         }, 800, function(){});
     });
-
-
-
-
-
-
 });
